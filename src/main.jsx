13035c0e5363421a9696e155ea1d0e5bc50b321b
Dinesh <reedydinesh@gmail.com>
2025-08-20 23:33:14 -0400

    Clean deploy: HashRouter + correct Vite base

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
@@ -10,5 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
